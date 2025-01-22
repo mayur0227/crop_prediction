2d576dbe89abd6dc9b8186d4018fe3ebaa1e00c0
mayur0227 <160587927+mayur0227@users.noreply.github.com>
2025-01-22 22:18:36 +0530

    Update modelfile.py

--- modelfile.py
+++ modelfile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+
 import pickle
 
 st.title('Crop Prediction Based on Soil and Weather Conditions')
